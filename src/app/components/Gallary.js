9afca61e1b9e9ecbfb7b8a2fec26b65f931381d7
sahidularif <sahidularif1@gmail.com>
2024-12-11 17:07:43 +0400

    last upadate 2024

--- src/app/components/Gallary.js
+++ src/app/components/Gallary.js
@@ -22,7 +22,7 @@ const Gallary = () => {
                                     <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                                         <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracki uppercase hover:underline bg-[#E6BE8A] text-black">Luxury</a>
                                         <div className="flex flex-col justify-start text-center text-[#FFF0E9]">
-                                            <span className="leadi uppercase">400 AED</span>
+                                            {/* <span className="leadi uppercase">400 AED</span> */}
                                         </div>
                                     </div>
                                     <h2 className="z-10 p-5">
@@ -34,43 +34,43 @@ const Gallary = () => {
                                     <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                                         <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracki uppercase hover:underline bg-[#E6BE8A] text-black">Arrangement</a>
                                         <div className="flex flex-col justify-start text-center text-[#FFF0E9]">
-                                            <span className="leadi uppercase">120 AED</span>
+                                            {/* <span className="leadi uppercase">120 AED</span> */}
                                         </div>
                                     </div>
                                     <h2 className="z-10 p-5">
                                         <a rel="noopener noreferrer" href="#" className="font-medium text-md group-hover:underline text-[#FFF0E9]">Vibrant Energy mix floral Arrangement</a>
                                     </h2>
                                 </div>
-                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('https://rosebud.qodeinteractive.com/wp-content/uploads/2018/02/gallery-img-4.jpg')]">
+                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('/assets/whatsapp_flower/IMG-20241211-WA0002.jpg')]">
                                     <div className="absolute top-0 bottom-0 left-0 right-0 dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
                                     <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                                         <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracki uppercase hover:underline bg-[#E6BE8A] text-black">Bouquet</a>
                                         <div className="flex flex-col justify-start text-center text-[#FFF0E9]">
-                                            <span className="leadi uppercase">90 AED</span>
+                                            {/* <span className="leadi uppercase">90 AED</span> */}
                                         </div>
                                     </div>
                                     <h2 className="z-10 p-5">
                                         <a rel="noopener noreferrer" href="#" className="font-medium text-md group-hover:underline text-[#FFF0E9]">Wax Flowers arrangement</a>
                                     </h2>
                                 </div>
-                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('https://rosebud.qodeinteractive.com/wp-content/uploads/2018/02/h2-gallery-img-2.jpg')]">
+                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('/assets/whatsapp_flower/IMG-20241211-WA0003.jpg')]">
                                     <div className="absolute top-0 bottom-0 left-0 right-0 dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
                                     <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                                         <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracki uppercase hover:underline bg-[#E6BE8A] text-black">Romantic</a>
                                         <div className="flex flex-col justify-start text-center text-[#FFF0E9]">
-                                            <span className="leadi uppercase">100 AED</span>
+                                            {/* <span className="leadi uppercase">100 AED</span> */}
                                         </div>
                                     </div>
                                     <h2 className="z-10 p-5">
                                         <a rel="noopener noreferrer" href="#" className="font-medium text-md group-hover:underline text-[#FFF0E9]">Two Dozen Pink Roses with Orchids Hat Box</a>
                                     </h2>
                                 </div>
-                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('https://rosebud.qodeinteractive.com/wp-content/uploads/2018/02/port-img-9-550x550.jpg')] " >
+                                <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500 bg-[url('/assets/whatsapp_flower/IMG-20241211-WA0005.jpg')] " >
                                     <div className="absolute top-0 bottom-0 left-0 right-0 dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
                                     <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                                         <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracki uppercase hover:underline bg-[#E6BE8A] text-black">Orchids</a>
                                         <div className="flex flex-col justify-start text-center text-[#FFF0E9]">
-                                            <span className="leadi uppercase">120 AED</span>
+                                            {/* <span className="leadi uppercase">120 AED</span> */}
                                         </div>
                                     </div>
                                     <h2 className="z-10 p-5">
--- src/app/components/admin/addFlower.js
+++ src/app/components/admin/addFlower.js
@@ -72,7 +72,7 @@ export default function AddFlowers() {
                         <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                             <div className="sm:col-span-2">
                                 <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
-                                <Field type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required="" />
+                                <Field type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Flower name" required="" />
                                 <ErrorMessage
                                     name="title"
                                     component="div"
